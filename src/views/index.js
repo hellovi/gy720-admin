59e0b3a1d63302dd5c16f1e65795b144141ed954
Victor <a-thok@outlook.com>
2017-08-09 15:21:56 +0800

    调整目录结构

--- src/views/index.js
+++ src/views/index.js
@@ -1,40 +1,40 @@
 export { default as User } from './User'
 
 // 个人中心
-export { default as Center } from './Center'
-export { default as CenterRecent } from './Center/Recent'
-export { default as CenterCollection } from './Center/Collection'
-export { default as CenterFans } from './Center/Fans'
-export { default as CenterFollows } from './Center/Follows'
-export { default as CenterStatistics } from './Center/Statistics'
+export { default as Center } from './User/Center'
+export { default as CenterRecent } from './User/Center/Recent'
+export { default as CenterCollection } from './User/Center/Collection'
+export { default as CenterFans } from './User/Center/Fans'
+export { default as CenterFollows } from './User/Center/Follows'
+export { default as CenterStatistics } from './User/Center/Statistics'
 
 // 我的作品
-export { default as Works } from './Works'
+export { default as Works } from './User/Works'
 
 // 购买服务
-export { default as Purchase } from './Purchase'
-export { default as PurchaseOrders } from './Purchase/Orders'
+export { default as Purchase } from './User/Purchase'
+export { default as PurchaseOrders } from './User/Purchase/Orders'
 
 // 账号管理
-export { default as Account } from './Account'
-export { default as AccountInfo } from './Account/Info'
-export { default as AccountPassword } from './Account/Password'
-export { default as AccountBind } from './Account/Bind'
+export { default as Account } from './User/Account'
+export { default as AccountInfo } from './User/Account/Info'
+export { default as AccountPassword } from './User/Account/Password'
+export { default as AccountBind } from './User/Account/Bind'
 
 // 我的消息
-export { default as Message } from './Message'
-export { default as MessageSystem } from './Message/System'
-export { default as MessagePrivate } from './Message/Private'
-export { default as MessageSay } from './Message/Say'
+export { default as Message } from './User/Message'
+export { default as MessageSystem } from './User/Message/System'
+export { default as MessagePrivate } from './User/Message/Private'
+export { default as MessageSay } from './User/Message/Say'
 
 // 我的积分
-export { default as Point } from './Point'
-export { default as PointTask } from './Point/Task'
-export { default as PointExchange } from './Point/Exchange'
-export { default as PointExchangeRule } from './Point/Exchange/Rule'
-export { default as PointExchangeRecord } from './Point/Exchange/Record'
-export { default as PointLog } from './Point/Log'
-export { default as PointRule } from './Point/Rule'
+export { default as Point } from './User/Point'
+export { default as PointTask } from './User/Point/Task'
+export { default as PointExchange } from './User/Point/Exchange'
+export { default as PointExchangeRule } from './User/Point/Exchange/Rule'
+export { default as PointExchangeRecord } from './User/Point/Exchange/Record'
+export { default as PointLog } from './User/Point/Log'
+export { default as PointRule } from './User/Point/Rule'
 
 // 发布作品
-export { default as Publish } from './Publish'
+export { default as Publish } from './User/Publish'
