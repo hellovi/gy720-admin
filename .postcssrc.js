935439663fc72822796412274dc0c393773f4774
Victor <a-thok@outlook.com>
2017-08-09 16:17:37 +0800

    修复windows下的报错

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   "plugins": {
     "postcss-import": {
-      addModulesDirectories: [
+      path: [
         path.join(__dirname, 'src/styles')
       ],
     },
