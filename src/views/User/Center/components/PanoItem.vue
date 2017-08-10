04ae355dde07edc08ebef5578607bc8990b2e101
huojinzhao <352782712@qq.com>
2017-08-10 13:41:19 +0800

    个人中心：作品组件，项目迁移后代码优化调整

--- src/views/User/Center/components/PanoItem.vue
+++ src/views/User/Center/components/PanoItem.vue
@@ -1,50 +1,51 @@
 <template>
-  <a class="center-panoItem item-common has-shadow"
-    target="_blank"
-    :href="panoPath(pano.hash_pano_id)"
-    :title="pano.pano_name"
-  >
+  <div class="center-panoitem has-shadow">
 
-    <!-- 作品图片 -->
-    <div class="img-square bg-light">
-      <img :src="pano.thumb" class="zoom-in" />
-    </div>
+    <!-- 作品封面 -->
+    <a class="center-panoitem__cover"
+      target="_blank"
+      :href="panoPath"
+      :title="pano.pano_name"
+    >
+      <img :src="pano.thumb"/>
+    </a>
 
     <!-- 作者头像 | 作品名 -->
-    <div class="center-panoItem__info">
-      <a class="center-panoItem__info__avatar"
-        target="_blank" :href="userPath(pano.hash_user_id)"
+    <div class="center-panoitem__info">
+      <a class="center-panoitem__info__avatar"
+        target="_blank"
+        :href="userPath"
         :title="pano.nickname + '的主页'"
       >
-        <div class="img-circle bg-gary-light zoom-in">
-          <img :src="pano.avatar" />
-        </div>
+        <img
+          class="center-panoitem__info__avatar-img"
+          :alt="`作品：${pano.pano_name}`"
+          :src="pano.avatar"/>
       </a>
 
-      <div class="center-panoItem__info__panoName ellipsis">
+      <div class="center-panoitem__info__panoname ellipsis">
         {{pano.pano_name}}
       </div>
     </div>
 
     <!-- 浏览数 | 点赞数 -->
-    <div class="center-panoItem__icons text-center">
-      <div class="center-panoItem__icons--eye left">
+    <div class="center-panoitem__icons">
+      <div class="center-panoitem__icons--eye">
         <i class="iconfont">&#xe63d;</i>{{pano.hits}}
       </div>
 
-      <div class="center-panoItem__icons--thumb right">
+      <div class="center-panoitem__icons--thumb">
         <i class="iconfont">&#xe641;</i>{{pano.like_nums}}
       </div>
 
-      <router-link
-        :to="`/make-client/edit?pano_id=${pano.id}`"
-        class="tag bg-warning has-shadow"
-        title="前往高级编辑"
+      <el-button
+        size='mini' type="warning"
+        @click.stop="toPanoEdit"
       >
         高级编辑
-      </router-link>
+      </el-button>
     </div>
-  </a>
+  </div>
 </template>
 
 <script>
@@ -63,12 +64,18 @@ export default {
     },
   },
 
-  methods: {
-    userPath(hashUserId) {
-      return this.$url.host(`author/${hashUserId}`)
+  computed: {
+    userPath() {
+      return this.$url.host(`author/${this.pano.hashUserId}`)
     },
-    panoPath(hashPanoId) {
-      return this.$url.host(`/pano/view/${hashPanoId}`)
+    panoPath() {
+      return this.$url.host(`/pano/view/${this.pano.hashPanoId}`)
+    },
+  },
+
+  methods: {
+    toPanoEdit() {
+      this.$router.push(`/make-client/edit?pano_id=${this.pano.id}`)
     },
   },
 }
@@ -77,27 +84,54 @@ export default {
 <style>
 
 @import "vars.css";
+@import "../style/mixins.css";
 
-.center-panoItem {
+.center-panoitem {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  background-color: var(--color-white);
+
   &:hover {
-    & .panoItem__icons {
+    & .center-panoitem__icons {
       transform: translateY(0);
     }
   }
 
+  &__cover {
+    @include contain-img-cover;
+
+    display: block;
+    margin-bottom: 34px;
+    width: 100%;
+
+    & > img {
+      @include zoom-in;
+    }
+  }
+
   &__info {
-    position: relative;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
     height: 34px;
 
     &__avatar {
+      @include contain-img-cover;
+
       position: absolute;
       bottom: 8px;
       left: 8px;
       width: 44px;
       height: 44px;
+      background: none;
+
+      &-img {
+        border-radius: 50%;
+      }
     }
 
-    &__panoName {
+    &__panoname {
       padding-left: 62px;
       padding-right: 10px;
       line-height: 34px;
@@ -110,13 +144,14 @@ export default {
     left: 0;
     padding: 6px;
     width: 100%;
+    text-align: center;
     transition: all .2s ease-out;
     transform: translateY(-100%);
 
     &>div {
       padding: 0 8px;
       border-radius: 4px;
-      line-height: 20px;
+      line-height: 19px;
       font-size: 12px;
 
       & i {
@@ -125,26 +160,18 @@ export default {
       }
     }
 
-    & .tag {
-      border-radius: 4px;
-      line-height: 23px;
-      transition: all .15s ease-out;
-
-      &:hover {
-        background-color: #ffc000 !important;
-      }
-    }
-
     &--eye {
-      border: 1px solid #d2d2d2;
-      background-color: rgba(255, 255, 255, .6);
-      color: #666;
+      float: left;
+      border: 1px solid var(--color-light-gray);
+      background-color: color(var(--color-extra-light-gray) alpha(60%));
+      color: color(--gray-light);
     }
 
     &--thumb {
-      border: 1px solid #288ee4;
-      background-color: rgba(40, 142, 228, .65);
-      color: #ffffff;
+      float: right;
+      border: 1px solid var(--color-primary);
+      background-color: color(var(--color-info) alpha(60%));
+      color: var(--color-white);
     }
   }
 }
