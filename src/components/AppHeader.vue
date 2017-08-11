a76e34175ba55382af3b9b5da18ec5683c2fac7c
zhoumenglin <512648622@qq.com>
2017-08-11 09:59:06 +0800

    退出登录链接

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -34,7 +34,7 @@
           <li><router-link to="/user-client/purchase">购买服务</router-link></li>
           <li><router-link to="/user-client/account">账号管理</router-link></li>
           <li><router-link to="/user-client/message">我的消息</router-link></li>
-          <li><a href="/">退出登陆</a></li>
+          <li><a href="/user/auth/signout?client=web">退出登陆</a></li>
         </ul>
       </div>
     </div>
