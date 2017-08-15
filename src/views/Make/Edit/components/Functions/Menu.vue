76afe122dfdd4f6979808f8a816e7e237fe6a44d
huojinzhao <352782712@qq.com>
2017-08-15 16:00:49 +0800

    高级编辑：菜单编辑功能文件组织结构调整

--- src/views/Make/Edit/components/Functions/Menu.vue
+++ src/views/Make/Edit/components/Functions/Menu.vue
@@ -1,10 +1,10 @@
 <template>
   <el-dialog
-    title="rightMenu"
-    :visible.sync="active.rightMenu"
+    title="bottomMenu"
+    :visible.sync="active.menu"
     size="tiny"
   >
-      <span>rightMenu</span>
+      <span>bottomMenu场景</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -14,14 +14,14 @@
 
 <script>
 /**
- * 高级编辑 - 场景
+ * 高级编辑 - menu
  * @version 2017-08-14
  */
 
 import modal from '../../mixins/modal'
 
 export default {
-  name: 'edit-functions__rightMenu',
+  name: 'edit-functions__bottomMenu',
 
   mixins: [modal],
 }
