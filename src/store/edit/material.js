c7db498ca72f908cde5735dbbf100ef2cd1cabd8
yangjun <anchorgoogle@163.com>
2017-08-17 16:15:13 +0800

    素材store

--- src/store/edit/material.js
+++ src/store/edit/material.js
@@ -5,7 +5,39 @@ const { MATERIAL } = EDIT
 
 export default {
   state: {
-    active: false,
+    materialState: {
+      // 控制弹窗显示
+      active: 0,
+      // 素材类型 1-6为普通素材，7图文，8物品3d，9音频，10，其他
+      type: 1,
+      // 选择按钮状态
+      selectStatus: 0,
+      // 唤起来源
+      selectFrom: '',
+    },
+    // 导出的数据
+    materialExport: {
+      // 图文素材data(右侧菜单)
+      menu: {
+        title: '',
+        id: 0,
+      },
+      // 图文素材data(热点设置)
+      hotspot: {
+        title: '',
+        id: 0,
+      },
+      // 物品3D素材(热点设置)
+      hotspot3d: {
+        title: '',
+        id: 0,
+      },
+      // 普通素材data(微信设置)
+      wechat: {
+        url: '',
+        id: 0,
+      },
+    },
   },
 
   mutations: {
