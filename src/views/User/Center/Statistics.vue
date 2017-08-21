086295221646e032dc721cd53bc4a10e4b311582
huojinzhao <352782712@qq.com>
2017-08-21 16:40:46 +0800

    feat(Statistic): 样式调整

--- src/views/User/Center/Statistics.vue
+++ src/views/User/Center/Statistics.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="statistics">
+
     <div class="statistics__line">
       <div class="statistics__inner">
-        <h4 class="statistics__title">最近一个月作品浏览数据统计</h4>
-        <div class="statistics__lineChart" ref="lineChart"></div>
+        <h4 class="statistics__title">
+          最近一个月作品浏览数据统计
+        </h4>
+        <div
+          class="statistics__lineChart"
+          ref="lineChart"
+        ></div>
       </div>
     </div>
+
     <div class="statistics__map">
       <div class="statistics__inner">
-        <h4 class="statistics__title">区域数据统计</h4>
-        <div class="statistics__mapChart" ref="mapChart"></div>
+        <h4 class="statistics__title">
+          区域数据统计
+         </h4>
+        <div
+          class="statistics__mapChart"
+          ref="mapChart"
+        ></div>
       </div>
     </div>
+
     <div class="statistics__pie">
       <div class="statistics__inner">
-        <h4 class="statistics__title">访问省份TOP5</h4>
-        <div class="statistics__pieChart" ref="pieChart"></div>
+        <h4 class="statistics__title">
+          访问省份TOP5
+        </h4>
+        <div
+          class="statistics__pieChart"
+          ref="pieChart"
+        ></div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -60,12 +79,10 @@ export default {
 <style>
 @import "vars.css";
 
-:root {
-  --statChartBelow {
-    border-top: 1px solid var(--app-bg);
-    height: 540px;
-    padding: 15px;
-  }
+@mixin statChartBelow {
+  border-top: 1px solid var(--app-bg);
+  height: 540px;
+  padding: 15px;
 }
 
 .statistics {
@@ -75,15 +92,16 @@ export default {
   font-size: 0;
 
   &__inner {
-    border: 1px solid var(--light);
+    border: 1px solid var(--gray-light);
     border-radius: 5px;
     overflow: auto;
   }
 
   &__line {
     margin-bottom: 30px;
+
     &Chart {
-      border-top: 1px solid var(--light);
+      border-top: 1px solid var(--gray-light);
       height: 430px;
       padding: 15px;
     }
@@ -93,24 +111,26 @@ export default {
     display: inline-block;
     margin-right: 3%;
     width: 57%;
+
     &Chart {
       position: relative;
-      @apply --statChartBelow;
+      @include statChartBelow;
     }
   }
 
   &__pie {
     display: inline-block;
     width: 40%;
+
     &Chart {
-      @apply --statChartBelow;
+      @include statChartBelow;
     }
   }
 
   &__title {
     margin: 0;
     padding-left: 15px;
-    background-color: var(--app-bg);
+    background-color: var(--gray-extra-light);
     line-height: 40px;
     font-size: 14px;
   }
