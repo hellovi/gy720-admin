94de7f071c5b63efe9802a2f35b0c651fd3dc45d
yangjun <anchorgoogle@163.com>
2017-08-24 11:47:55 +0800

    feat(Material): 素材上传

--- src/views/Make/Edit/components/Functions/Material/components/Images.vue
+++ src/views/Make/Edit/components/Functions/Material/components/Images.vue
@@ -18,6 +18,18 @@
           </el-card>
         </el-col>
      </el-row>
+     <section class="edit-functions__material__upload">
+       <!-- <el-button type="primary" size="small" @click="uploadMaterial">上传新素材</el-button> -->
+      <app-file-upload
+        v-model="src"
+        ref="fileUpload"
+        accept="jpg,jpeg,png"
+        size="1mb"
+      >
+          <el-button type="primary" size="small" @click="uploadMaterial">上传新素材</el-button>
+       </app-file-upload>
+       <span>支持的文件格式：jpg,jpeg,png,gif,mp3 文件大小：15M以内</span>
+     </section>
    </div>
 </template>
 
@@ -27,30 +39,42 @@
  * @version 2017-08-21
  */
 import { mapState } from 'vuex'
+import { EDIT } from '@/store/mutationTypes'
+import AppFileUpload from '@/components/AppFileUpload'
 
 export default {
   name: 'edit-functions__material--images',
 
+  components: { AppFileUpload },
+
   props: {
-    type: {
+    currentId: {
       type: Number,
       default: 1,
+    },
+    normalMaterial: {
+      type: Array,
     },
   },
 
   data() {
     return {
-
+      src: '',
     }
   },
 
   computed: {
     ...mapState({
-      selectStatus: state => state.edit.material.materialState.selectStatus,
+      type: state => state.edit.material.type,
+      selectStatus: state => state.edit.material.selectStatus,
     }),
 
     dataList() {
       return this.$store.state.edit.material.materialData[this.type]
+    },
+
+    params() {
+      return `?tag_id=${this.currentId}`
     },
   },
 
@@ -60,13 +84,27 @@ export default {
       this.$store.dispatch('edit/material/select', { id, url })
       this.$store.dispatch('edit/material/destroy')
     },
+
+    uploadMaterial() {
+
+    },
+
+    initMaterial(page) {
+      const params = page ? `${this.params}&page=${page}` : this.params
+      this.$store.dispatch(EDIT.MATERIAL.INIT.NORMALS, params)
+    },
   },
+
+  created() {
+    this.initMaterial(1)
+  },
+
 }
 </script>
 
 <style lang="postcss">
+@import "vars.css";
 .edit-functions__material {
-
   &__data {
     .el-card {
       text-align: center;
@@ -75,15 +113,28 @@ export default {
 
     .bottom {
       position: relative;
-      & >h6 {
+
+      & > h6 {
         font-size: 12px;
-        margin: 10px auto 0;
+        margin: 10px auto 10px;
       }
     }
   }
 
   &__image {
     max-height: 40px;
+  }
+
+  &__upload {
+    margin-top: 20px;
+
+    .el-button {
+      margin-right: 10px;
+    }
+
+    & > span {
+      color: var(--gray);
+    }
   }
 }
 </style>
