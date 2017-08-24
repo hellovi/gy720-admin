e6683afaa22da8e997d8db75217bae5c4e079ea8
zhoumenglin <512648622@qq.com>
2017-08-24 18:18:48 +0800

    feat(Certificate):摄影师认证结果页

--- src/views/User/Certificate/Result.vue
+++ src/views/User/Certificate/Result.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
 
-    <div class="cert-result">
+    <div class="cert-result" v-if="certStatus(10)">
       <h3 class="cert-result__title">申请提交成功</h3>
       <p>您已经成功提交认证申请，工作人员将在3个工作日内处理</p>
       <p>申请需求，请耐心等候</p>
       <el-button type="primary" @click="goTo('/user-client/center')">返回个人中心</el-button>
     </div>
 
-    <div class="cert-result">
+    <div class="cert-result" v-if="certStatus(20)">
       <h3 class="cert-result__title">认证失败</h3>
       <p>您的认证申请经审核未能通过</p>
-      <p>原因如下：</p>
+      <p>原因如下：{{ certInfo.certificate.note }}</p>
       <el-button type="primary" @click="goTo('/user-client/certificate/apply')">重新认证</el-button>
     </div>
 
-    <div class="cert-result">
+    <div class="cert-result" v-if="certStatus(30)">
       <h3 class="cert-result__title">认证成功</h3>
       <p>您的认证申请已通过审核，恭喜您已正式成为光鱼认证摄影师！</p>
       <p>&nbsp;</p>
@@ -40,9 +40,20 @@
 export default {
   name: 'certifate-result',
 
+  props: {
+    certInfo: {
+      type: Object,
+      required: 'true',
+    },
+  },
+
   methods: {
     goTo(url) {
       this.$router.push(url)
+    },
+
+    certStatus(status) {
+      return this.certInfo.certificate && this.certInfo.certificate.certificate_status === status
     },
   },
 
