22bf2910d4873a8baa298de248d8b6c0626f229e
chenliangshan <729755202@qq.com>
2017-08-25 15:21:57 +0800

    feat(Ueditor): 调用异步加载脚本库方法

--- src/views/Make/Edit/components/Functions/components/Ueditor.vue
+++ src/views/Make/Edit/components/Functions/components/Ueditor.vue
@@ -8,11 +8,8 @@
  * @author yangjun
  */
 
-import Vue from 'vue'
+import { dynamicLoadScript } from '@/utils'
 import './ueditor.config'
-
-// ueditor库的资源地址
-const FULL_PATH = Vue.prototype.$url.host('assets/3.0.1/lib/ueditor/1.4.3/ueditor.all.min.js')
 
 export default {
   props: {
@@ -31,44 +28,35 @@ export default {
   //   },
   // },
   methods: {
-    /**
-     * 动态加载ueditor
-     */
-    dynamicLoadUeditor(cb) {
-      const ueditorScript = document.querySelector(`script[src="${FULL_PATH}"]`)
-      if (ueditorScript) {
-        // 已经加载过ueditor库，直接执行实例初始化
-        cb()
-      } else {
-        // 动态写入script标签加载ueditor库，确保在库加载完成后才初始化ueditor实例
-        const script = document.createElement('script')
-        script.src = FULL_PATH
-        script.addEventListener('load', cb)
-        document.body.appendChild(script)
-      }
+    dynamicLoadUeditor() {
+      /**
+       * 动态加载ueditor
+       */
+      dynamicLoadScript('assets/3.0.1/lib/ueditor/1.4.3/ueditor.all.min.js')
+        .then(() => {
+          this.editor = window.UE.getEditor('editor', {
+            autoClearinitialContent: true, // focus时自动清空初始化时的内容
+            wordCount: false, // 关闭字数统计
+            elementPathEnabled: false, // 关闭elementPath
+            initialFrameHeight: 350, // 默认的编辑区域高度
+            // initialFrameWidth: null,
+          })
+          this.editor.ready(() => {
+            this.editor.setContent(this.value)
+            this.editor.addListener('contentChange', () => {
+              const wordCount = this.editor.getContentLength(true)
+              const content = this.editor.getContent()
+              const plainTxt = this.editor.getPlainTxt()
+              this.$emit('contentChange', { wordCount, content, plainTxt })
+            })
+            this.$emit('ueReady', this.editor)
+          })
+        })
     },
   },
 
   mounted() {
-    this.dynamicLoadUeditor(() => {
-      this.editor = window.UE.getEditor('editor', {
-        autoClearinitialContent: true, // focus时自动清空初始化时的内容
-        wordCount: false, // 关闭字数统计
-        elementPathEnabled: false, // 关闭elementPath
-        initialFrameHeight: 350, // 默认的编辑区域高度
-        // initialFrameWidth: null,
-      })
-      this.editor.ready(() => {
-        this.editor.setContent(this.value)
-        this.editor.addListener('contentChange', () => {
-          const wordCount = this.editor.getContentLength(true)
-          const content = this.editor.getContent()
-          const plainTxt = this.editor.getPlainTxt()
-          this.$emit('contentChange', { wordCount, content, plainTxt })
-        })
-        this.$emit('ueReady', this.editor)
-      })
-    })
+    this.dynamicLoadUeditor()
   },
 
   beforeDestroy() {
