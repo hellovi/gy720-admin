a2058e9d8955341346cf20d849e51fe784eb4724
chenli <chenli_1989@foxmail.com>
2017-08-27 17:44:19 +0800

    feat(TopRight): 新增视角切换功能

--- src/views/Make/Edit/components/Controls/TopRight.vue
+++ src/views/Make/Edit/components/Controls/TopRight.vue
@@ -2,7 +2,23 @@
   <div class="edit-control__top-right">
     <el-button type="primary" @click="openModal('setting')">设置</el-button>
     <el-button type="primary" @click="openModal('material')">素材库</el-button>
-    <el-button type="primary">视角</el-button>
+    <el-dropdown
+      @command="gView"
+      menu-align="start"
+    >
+      <el-button type="primary">视角</el-button>
+      <el-dropdown-menu
+        slot="dropdown"
+        class="gview-list"
+      >
+        <el-dropdown-item
+          v-for="(item,index) in gviewList"
+          :key="index"
+          :command="item.value"
+          :class="{'gview-list__active': gviewActive(item.value)}"
+        >{{ item.name }}</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
     <el-button type="primary">皮肤</el-button>
   </div>
 </template>
@@ -20,6 +36,26 @@ export default {
   name: 'edit-right-middle',
 
   mixins: [modal],
+
+  data: () => ({
+    currentGview: 'normal',
+    gviewList: [
+      { name: '普通视角', value: 'normal' },
+      { name: '小 行 星', value: 'littleplanet' },
+      { name: '鱼眼视图', value: 'fisheye' },
+    ],
+  }),
+
+  methods: {
+    gView(action) {
+      this.currentGview = action
+      /* eslint no-underscore-dangle: 0 */
+      window.__krpano.call(`ac_${action}_view()`)
+    },
+    gviewActive(action) {
+      return this.currentGview === action
+    },
+  },
 }
 </script>
 
@@ -35,6 +71,16 @@ export default {
     & + .el-button {
       margin-left: 20px;
     }
+  }
+}
+
+.gview-list {
+  min-width: auto;
+  text-align: center;
+
+  &__active {
+    background-color: rgba(255, 136, 83, .8);
+    color: #fff;
   }
 }
 </style>
