374d605bb457730381097fec6526e2dc43ac6d65
yangjun <anchorgoogle@163.com>
2017-08-30 17:04:33 +0800

    feat(Material): 素材无数据以及音频分类

--- src/views/Make/Edit/components/Functions/Material/components/Images.vue
+++ src/views/Make/Edit/components/Functions/Material/components/Images.vue
@@ -2,7 +2,7 @@
    <div class="edit-functions__material__wrap">
      <el-row :gutter="10" class="edit-functions__material__data"  v-loading="loading">
         <el-col :span="4" v-for="(item, index) in dataList" :key="item.id">
-          <el-card :body-style="{ padding: '0px' }">
+          <el-card v-if="item.file_ext !== 'mp3'" :body-style="{ padding: '0px' }">
             <img :src="$url.static(item.file_path)" class="edit-functions__material__image"/>
             <div>
               <div class="bottom clearfix">
@@ -16,15 +16,43 @@
               </div>
             </div>
           </el-card>
+          <!--音频格式的-->
+          <el-card v-else :body-style="{ padding: '0px' }">
+            <el-button type="text" @click="">试听</el-button>
+            <el-dialog>
+              <audio controls="controls">
+                Your browser does not support the <code>audio</code> element.
+               <source :src="$url.static(item.file_path)" type="audio/mp3">
+            </audio>
+            </el-dialog>
+
+            <div>
+              <div class="bottom clearfix">
+                <h6>{{ item.title }}</h6>
+                <el-button
+                  type="text" class="button"
+                  v-show="selectStatus"
+                  @click="selectMater(item.id, item.file_path)"
+                >选择
+                </el-button>
+              </div>
+            </div>
+          </el-card>
+
         </el-col>
+
+        <div v-if="isEmpty" class="empty">
+          <div>还没有上传素材哦……</div>
+        </div>
+
      </el-row>
+
      <section class="edit-functions__material__upload">
       <app-file-upload
         v-model="src"
         ref="fileUpload"
-        accept="jpg,jpeg,png"
-        size="1mb"
-        auto-start
+        accept="jpg,jpeg,png,gif,mp3"
+        @file-uploaded="addMaterial"
       >
           <el-button type="primary" size="small">上传新素材</el-button>
        </app-file-upload>
@@ -40,7 +68,11 @@
  */
 import { mapState } from 'vuex'
 import { EDIT } from '@/store/mutationTypes'
-import AppFileUpload from '@/components/AppFileUpload'
+// import AppFileUpload from '@/components/AppFileUpload'
+// import AppUploadProgress from '@/components/AppUploadProgress'
+
+// 异步加载
+const AppFileUpload = () => import('@/components/AppFileUpload')
 
 export default {
   name: 'edit-functions__material--images',
@@ -74,12 +106,9 @@ export default {
     params() {
       return `?tag_id=${this.currentId}`
     },
-  },
 
-  watch: {
-    src(val) {
-      // 添加素材,要改成监听process触发
-      this.addMaterial(val)
+    isEmpty() {
+      return !this.loading && (!this.dataList || !this.dataList.length)
     },
   },
 
@@ -90,12 +119,12 @@ export default {
       this.$store.commit(EDIT.MODAL.CLOSE, 'material')
     },
 
-    addMaterial(qiniu_key) {
+    addMaterial(up, file) {
       const data = {
-        title: '文件名',
+        title: file.name,
         tag_id: this.currentId,
-        qiniu_key,
-        file_size: 1000,
+        qiniu_key: this.src,
+        file_size: file.size,
       }
       this.$store.dispatch(EDIT.MATERIAL.ADD, data)
     },
@@ -103,7 +132,7 @@ export default {
     initMaterial(page) {
       this.loading = true
       const params = page ? `${this.params}&page=${page}` : this.params
-      this.$store.dispatch(EDIT.MATERIAL.INIT.NORMALS, params)
+      this.$store.dispatch(EDIT.MATERIAL.INIT.NORMALS, { params })
         .then(() => {
           this.loading = false
         })
@@ -118,38 +147,6 @@ export default {
 </script>
 
 <style lang="postcss">
-@import "vars.css";
-.edit-functions__material {
-  &__wrap {
-    .el-card {
-      text-align: center;
-      padding-top: 10px;
-    }
 
-    .bottom {
-      position: relative;
 
-      & > h6 {
-        font-size: 12px;
-        margin: 10px auto 10px;
-      }
-    }
-  }
-
-  &__image {
-    max-height: 40px;
-  }
-
-  &__upload {
-    margin-top: 20px;
-
-    .el-button {
-      margin-right: 10px;
-    }
-
-    & > span {
-      color: var(--gray);
-    }
-  }
-}
 </style>
