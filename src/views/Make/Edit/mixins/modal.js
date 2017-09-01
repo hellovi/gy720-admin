29a5d6fb4277c4c65f6d25a805ca36f9d118000d
Victor <a-thok@outlook.com>
2017-09-01 16:42:00 +0800

    doc(modal.mixin): 补充注释

--- src/views/Make/Edit/mixins/modal.js
+++ src/views/Make/Edit/mixins/modal.js
@@ -1,8 +1,3 @@
-// eslint-disable-next-line
-/** @typedef {'panos'|'logos'|'tours'|'hotspots'|'icons'|'ads'|'thumbs'|'infos'|'objects'|'audios'|'others'} MaterialType - 素材类型 */
-// eslint-disable-next-line
-/** @typedef {'tour'|'wechat'|'menu'|'logos'|'hotspot'|'hotspot3d'|'audio'|'hotspotAudio'} InvokeSource - 调用来源 */
-
 import { mapState } from 'vuex'
 import { EDIT } from '@/store/mutationTypes'
 
@@ -21,6 +16,7 @@ export default {
     closeModal(name) {
       this.$store.commit(EDIT.MODAL.CLOSE, name)
 
+      /** 如果关闭的是素材框，应同时重置其状态 */
       if (name === 'material') {
         this.$store.commit(EDIT.MATERIAL.CHANGE, 'scene')
         this.$store.commit(EDIT.MATERIAL.INVOKE, false)
