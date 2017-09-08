e211c5fbbb60acef2dc3501b41b92b3c6e5847b7
Victor <a-thok@outlook.com>
2017-09-08 09:29:28 +0800

    feat(Material): 调整素材上传限制

--- src/views/Make/Edit/components/Functions/Material/components/uploadLimits.js
+++ src/views/Make/Edit/components/Functions/Material/components/uploadLimits.js
@@ -1,40 +1,40 @@
 export default {
   logo: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
   },
 
   map: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
-    dimension: w => w > 500,
-    dimensionTip: '图片宽度必须大于500像素',
+    dimension: w => w >= 500,
+    dimensionTip: '图片宽度必须大于等于500像素',
   },
 
   hotspot: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
     ratio: [1, 1],
   },
 
   wxicon: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
     ratio: [1, 1],
-    dimension: w => w > 200,
-    dimensionTip: '图片宽度必须大于200像素',
+    dimension: w => w >= 200,
+    dimensionTip: '图片宽度必须大于等于200像素',
   },
 
   ad: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
     ratio: [1, 1],
-    dimension: w => w > 300,
-    dimensionTip: '图片宽度必须大于300像素',
+    dimension: w => w >= 300,
+    dimensionTip: '图片宽度必须大于等于300像素',
   },
 
   thumb: {
-    mimeTypes: 'jpg,jpeg,png,gif',
+    mimeTypes: 'jpg,jpeg,png',
     size: '2MB',
     ratio: [1, 1],
     dimension: (w, h) => w === 200 && h === 200,
@@ -48,7 +48,7 @@ export default {
   },
 
   other: {
-    mimeTypes: 'jpg,jpeg,png,gif,mp3',
+    mimeTypes: 'jpg,jpeg,png,mp3',
     size: '15MB',
   },
 }
