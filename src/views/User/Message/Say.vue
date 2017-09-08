601b74f4560478cf700d2b18e22177e733d31c07
Victor <a-thok@outlook.com>
2017-09-08 10:46:14 +0800

    feat(Message): 说一说的初步接口调用

--- src/views/User/Message/Say.vue
+++ src/views/User/Message/Say.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div v-loading="listLoading">
     <message-control
       :allChecked="allChecked"
-      :someChecked="someChecked"
-      @checkAll="checkAll"
+      :someChecked="checked.length > 0"
+      :loading="loading"
+      @check-all="checkAll"
+      @remove-selected="removeMessage"
     ></message-control>
 
     <table class="app-table">
@@ -19,7 +21,7 @@
         <tr>
           <th></th>
           <th class="text-left">消息内容</th>
-          <th>作品</th>
+          <th>场景</th>
           <th>时间</th>
           <th>显示状态</th>
           <th>操作</th>
@@ -27,30 +29,34 @@
       </thead>
       <tbody>
         <tr v-for="(message, index) in list.data" :key="message.id">
-          <td><el-checkbox :value="message.checked" @change="handleCheck(index)"></el-checkbox></td>
+          <td><el-checkbox  :value="checked.includes(message.id)" @change="check(message.id)"></el-checkbox></td>
           <td class="text-left">
             <div class="message-avatar" data-count="2">
-              <img src="http://l-statics.gy720.com/data/avatar/20170101/471811501052670905.jpg" alt="用户名">
+              <img :src="$url.static(message.avatar)" :alt="message.nickname">
             </div>
             <div>
               <!-- 这里是否要跳链接 -->
-              <router-link to="/" class="hover-primary">用户名</router-link>
-              <div class="message-summary">{{ message.title }}</div>
+              <router-link to="/" class="hover-primary">{{ message.nickname }}</router-link>
+              <div class="message-summary ellipsis" :title="message.content">{{ message.content }}</div>
             </div>
           </td>
-          <td>作品名称</td>
+          <td>{{ message.name }}</td>
           <td>{{ message.created_at }}</td>
-          <td>正常</td>
+          <td>{{ message.show_status_name }}</td>
           <td>
             <el-button type="primary" size="small">隐藏</el-button>
-            <el-button type="danger" size="small">删除</el-button>
+            <el-button type="danger" size="small" @click="removeMessage(message.id)">删除</el-button>
           </td>
         </tr>
       </tbody>
     </table>
 
+    <div v-if="isEmpty" class="empty">
+      <div>您目前还没有收到任何说一说评论</div>
+    </div>
+
     <el-pagination
-      v-if="list.data.length"
+      v-if="list.last_page > 1"
       layout="prev, pager, next"
       :total="list.total"
       :current-page="list.current_page"
@@ -60,6 +66,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import { MESSAGE } from '@/store/mutationTypes'
 import { list } from '@/mixins'
 import check from './mixins/check'
@@ -76,17 +83,27 @@ export default {
 
   data() {
     return {
-      modal: false,
+      listLoading: false,
+      loading: -1,
     }
   },
 
+  computed: {
+    ...mapState({
+      list: state => state.message.say,
+    }),
+  },
+
   methods: {
-    openChatBox() {
-      this.modal = true
+    getData(route) {
+      this.listLoading = true
+      return this.$store.dispatch(MESSAGE.SAY.INIT, route.query.page)
+        .catch(({ status }) => this.$message.error(status.reason))
+        .then(() => { this.listLoading = false })
     },
 
-    getData(route) {
-      return this.$store.dispatch(MESSAGE.SYSTEM.INIT, route.query.page)
+    removeMessage(removeId) {
+      this.remove('say', '/user/panocomment/delete', removeId)
     },
   },
 }
