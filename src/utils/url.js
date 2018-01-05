b5390aa43a16e6d39793597d64ccb2a2e37f06fa
chenliangshan <729755202@qq.com>
2018-01-05 10:04:21 +0800

    fix(url): 修改域名

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,37 +1,28 @@
-/**
- * 开发环境：
- * 域名: https://l.gy720.com
- * CDN: http://l-statics.gy720.com
- *
- * 测试环境：
- * 域名: https://l-test.gy720.com
- * CDN: http://www-statics.gy720.com
- *
- * 正式环境：
- * 域名: https://www.gy720.com
- * CDN: http://www-statics.gy720.com
- *
- * @author chenliangshan
- * @version 2017-10-20
+/*
+ * @Author: chenliangshan
+ * @Date: 2018-01-05 10:02:51
+ * @Last Modified by:   chenliangshan
+ * @Last Modified time: 2018-01-05 10:02:51
  */
+
 import { Regex } from '@/utils'
 
 let configHost = {}
 const linkList = {
   // 开发环境
   dev: {
-    origin: 'https://l.gy720.com',
-    cdn: 'http://l-statics.gy720.com',
+    origin: 'https://l.gy720.com/',
+    cdn: 'http://l-statics.gy720.com/',
   },
   // 测试环境
   test: {
-    origin: 'https://l-test.gy720.com',
-    cdn: 'http://www-statics.gy720.com',
+    origin: 'https://l-test.gy720.com/',
+    cdn: 'http://www-statics.gy720.com/',
   },
   // 正式生产环境
   production: {
-    origin: 'https://www.gy720.com',
-    cdn: 'http://www-statics.gy720.com',
+    origin: 'https://www.gy720.com/',
+    cdn: 'http://www-statics.gy720.com/',
   },
 }
 const currentOrigin = window.location.origin
@@ -69,16 +60,18 @@ export default class Url {
   static static(pathname) {
     if (Regex.url(pathname) || Regex.base64(pathname)) {
       return pathname
+    } else if (pathname.includes('/assets/')) {
+      return `${window.location.origin}${pathname}`
     }
-    return /^\/.*$/.test(`${pathname}`) ? `${configHost.cdn}${pathname}` : `${configHost.cdn}/${pathname}`
+    return `${configHost.cdn}${pathname}`
   }
 
   static host(pathname) {
-    return `${configHost.origin}/${pathname}`
+    return `${configHost.origin}${pathname}`
   }
 
   static staticHotSpots(pathname) {
-    return `${configHost.origin}/assets/${process.env.ASSETS_VERSION}/lib/krpano/hoticon/${pathname}`
+    return `${configHost.origin}assets/${process.env.ASSETS_VERSION}/lib/krpano/hoticon/${pathname}`
   }
 
   static getQuery(url = window.location.href) {
